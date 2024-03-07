6d86ee03cb52b2b483de9a26d2a428cf0688b27f
hschiau <horia.schiau@multiversx.com>
2024-03-07 14:46:01 +0200

    fix unit test

--- libs/common/src/signer/specs/signer.service.spec.ts
+++ libs/common/src/signer/specs/signer.service.spec.ts
@@ -39,22 +39,20 @@ describe('SignerService', () => {
   });
 
   it('should generate a correct signature', async () => {
-    const callerAddress = 'erd1rwsq0fxjrce9955hvvl3qrpl96xmuuxch9m6wlhxx6zs0n2v3hvqyu4lm5';
-    const projectId = '1';
-    const week = 10;
+    const payload = 'test';
     const expectedSignature =
       // eslint-disable-next-line max-len
-      '418dc8a65f4bfe6b5d87aef6e5fbc6c0435ef963dbe882bb6cf7496f7898ca6d2314dc94f8e4f9267fa9a471abfc130c7d9e2a4e9039abd905fad2d1e1fd1006';
+      '7d664a006df0b52d8d3136afb9264ec270e5f551826848409d804b6ba1880782c04637be38ca3c214dd4f99dec2886d5f18d0dfbc2a31095efdb11c1b3bf0900';
 
     const verifier = UserVerifier.fromAddress(
       Address.fromString('erd1fxws0ralu6jkg563gzkhrsmuzwxn97t4wt4mphy598hwnakz6guqlqmve4'),
     );
 
-    const signedMessage = await service.signClaimPayload(callerAddress, projectId, week);
+    const signedMessage = await service.signPayload(payload);
 
     expect(signedMessage).toEqual(Buffer.from(expectedSignature, 'hex'));
     expect(verifier.verify(
-      Buffer.from('1ba007a4d21e3252d297633f100c3f2e8dbe70d8b977a77ee6368507cd4c8dd8000000010000000a', 'hex'),
+      Buffer.from('test', 'utf-8'),
       signedMessage),
     ).toBe(true);
   });
